21a72560158d3a697d986ee00e671e55a7d8d440
Olga-Vretto <olgavretto@outlook.com>
2023-05-25 23:51:53 +0300

    add Inputs.css file

--- src/components/Inputs/Inputs.js
+++ src/components/Inputs/Inputs.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import './Inputs.css';
 
-const Inputs = ({ fields }) => (
+const Inputs = ({ fields }) =>
   fields.map((field, index) => (
     <div key={index} className='input-box'>
       <label>{field.label}</label>
-      <input className='input' type={field.type} name={field.name} value={field.value} onChange={field.onChange} />
+      <input
+        className='input'
+        type={field.type}
+        name={field.name}
+        value={field.value}
+        onChange={field.onChange}
+      />
       {field.error && <p className='error'>{field.error}</p>}
     </div>
-  ))
-);
+  ));
 
-export default Inputs
+export default Inputs;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Main.css'
+import React from 'react';
+import './Main.css';
 
 const Main = () => {
   return (
     <div className='main'>
-       <h1 className='title'>Registration Form</h1> 
+      <h1 className='title'>Registration Form</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
