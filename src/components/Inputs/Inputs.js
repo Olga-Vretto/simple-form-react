49b73e63c6db61d7ae697e20f0ccbb9b77e22e8d
Olga-Vretto <olgavretto@outlook.com>
2023-05-25 23:48:25 +0300

    add changes

--- src/components/Inputs/Inputs.js
+++ src/components/Inputs/Inputs.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Inputs = ({ fields }) => (
-  fields.map((field, i) => (
-    <div key={i} className='input-box'>
+  fields.map((field, index) => (
+    <div key={index} className='input-box'>
       <label>{field.label}</label>
       <input className='input' type={field.type} name={field.name} value={field.value} onChange={field.onChange} />
       {field.error && <p className='error'>{field.error}</p>}
